ee072d54b8b7e084c1183a4042995a82f1c592e7
Md Shojib Hossain <mdshojib922@gmail.com>
2024-06-04 02:19:52 +0600

    Happy Sring Problem Solved

---
+++ Chef_and_Happy_String.cpp
@@ -0,0 +1,46 @@
+// Its_Shojib
+#include <bits/stdc++.h>
+using namespace std;
+#define mod 1000000007
+#define N 100005
+#define ll long long
+#define endl "\n"
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+long long fact[N];
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        string str;
+        cin >> str;
+        ll n = str.size();
+        ll ans = 0;
+        for (ll i = 0; i < n; i++)
+        {
+            if ((str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u'))
+            {
+                ans++;
+                if (ans == 3) break;
+            }
+            else{
+                ans = 0;
+            }
+        }
+        if (ans ==3)
+        {
+            cout << "Happy" << endl;
+        }
+        else
+        {
+            cout << "Sad" << endl;
+        }
+    }
+
+    return 0;
+}
