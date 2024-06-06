0430bb1d7f6b0f2668912bd1cb1bf5d68bebe74c
Md Shojib Hossain <mdshojib922@gmail.com>
2024-06-07 01:05:28 +0600

    Phone NUmber

--- P_09_Phone_Numbers.cpp
+++ P_09_Phone_Numbers.cpp
@@ -10,12 +10,24 @@ using namespace std;
 long long fact[N];
 int main(){
     ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-    ll t;
-    cin>>t;
-    while(t--){
-        
-
-
+    ll n;
+    cin >> n;
+    string str;
+    cin >> str;
+    ll eight = 0;
+    for(int i = 0; i < n;i++){
+        if(str[i] == '8'){
+            eight++;
+        }
     }
+    ll number = n/11;
+    if(number> eight){
+        cout<<eight<<endl;
+    }else if(number<eight){
+        cout<<number<<endl;
+    }else{
+        cout<<number<<endl;
+    }
+    
     return 0;
 }
