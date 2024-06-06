60995de5e28fad3e5b0a0c3945811dd55f3d235a
Md Shojib Hossain <mdshojib922@gmail.com>
2024-06-07 00:14:34 +0600

    Foobball Updated

--- P_05_Football.cpp
+++ P_05_Football.cpp
@@ -16,7 +16,7 @@ int main()
     string str;
     cin >> str;
     int n = str.size();
-    int cnt = 0;
+    int cnt = 1;
     for (int i = 0; i < n - 1; i++)
     {
         if (str[i] == str[i + 1])
@@ -26,7 +26,7 @@ int main()
                 break;
         }
         else
-            cnt = 0;
+            cnt = 1;
     }
     if (cnt >= 7)
         yes;
