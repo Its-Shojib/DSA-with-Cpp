e3b0c2b9bc99ad59c831c430ce82e1f1379287da
Md Shojib Hossain <mdshojib922@gmail.com>
2024-06-09 23:25:57 +0600

    Almost Lucky

--- P_15_Lucky_Division.cpp
+++ P_15_Lucky_Division.cpp
@@ -10,12 +10,18 @@ using namespace std;
 long long fact[N];
 int main(){
     ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-    ll t;
-    cin>>t;
-    while(t--){
-        
-
-
+    ll n;
+    cin >> n;
+    ll a[] = {4,7,44,47,74,77,444,447,474,477, 744,747,774,777};
+    ll almostLucky = 0;
+    for(int i = 0; i < 14; i++){
+        if(n % a[i] == 0){
+            almostLucky = 1;
+            break;
+        }
     }
+    if(almostLucky) yes;
+    else no;
+
     return 0;
 }
