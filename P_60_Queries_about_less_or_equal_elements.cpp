e2a94896c525fd099e19bcf25b777dac63c34806
Md Shojib Hossain <mdshojib922@gmail.com>
2024-06-24 17:06:38 +0600

    Print Updated

--- P_60_Queries_about_less_or_equal_elements.cpp
+++ P_60_Queries_about_less_or_equal_elements.cpp
@@ -5,8 +5,6 @@ using namespace std;
 #define N 100005
 #define ll long long
 #define endl "\n"
-#define yes cout << "YES\n"
-#define no cout << "NO\n"
 long long fact[N];
 int main(){
     ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
