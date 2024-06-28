86eb8b39a08f838b643f2a62cf24ba4b0315e5b4
Md Shojib Hossain <mdshojib922@gmail.com>
2024-06-28 09:55:46 +0600

    Donation Level

---
+++ Donation_Drive.cpp
@@ -0,0 +1,21 @@
+//Its_Shojib
+#include <bits/stdc++.h>
+using namespace std;
+#define mod 1000000007
+#define N 100005
+#define ll long long
+#define endl "\n"
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+long long fact[N];
+int main(){
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    ll t;
+    cin>>t;
+    while(t--){
+        ll x,y;
+        cin>>x>>y;
+        cout<<x-y<<endl;
+    }
+    return 0;
+}
