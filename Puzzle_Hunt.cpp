076ac446ade2f8d9a274c99d4816c9b5c5ed7c88
Md Shojib Hossain <mdshojib922@gmail.com>
2024-07-02 23:46:53 +0600

    Puzzle Hunt

---
+++ Puzzle_Hunt.cpp
@@ -0,0 +1,18 @@
+//Its_Shojib
+#include <bits/stdc++.h>
+using namespace std;
+#define mod 1000000007
+#define N 100005
+#define ll long long
+#define endl "\n"
+#define yes cout << "Yes\n"
+#define no cout << "No\n"
+long long fact[N];
+int main(){
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    ll x;
+    cin >> x;
+    if(x>=6 && x<=8) yes;
+    else no;
+    return 0;
+}
