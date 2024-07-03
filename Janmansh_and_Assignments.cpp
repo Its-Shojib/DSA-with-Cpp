99248eb4ddddaafc20465c7fe24f2853bcd5892a
Md Shojib Hossain <mdshojib922@gmail.com>
2024-07-04 00:25:31 +0600

    Assignment

---
+++ Janmansh_and_Assignments.cpp
@@ -0,0 +1,22 @@
+//Its_Shojib
+#include <bits/stdc++.h>
+using namespace std;
+#define mod 1000000007
+#define N 100005
+#define ll long long
+#define endl "\n"
+#define yes cout << "Yes\n"
+#define no cout << "No\n"
+long long fact[N];
+int main(){
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    ll t;
+    cin>>t;
+    while(t--){
+        ll x;
+        cin>>x;
+        if(x<=7) yes;
+        else no;
+    }
+    return 0;
+}
