cf654e1638f6cbadb55ffb416b54814ffc631aac
Md Shojib Hossain <mdshojib922@gmail.com>
2024-07-04 00:40:10 +0600

    Exams

---
+++ Exams.cpp
@@ -0,0 +1,26 @@
+//Its_Shojib
+#include <bits/stdc++.h>
+using namespace std;
+#define mod 1000000007
+#define N 100005
+#define ll long long
+#define endl "\n"
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+long long fact[N];
+int main(){
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    ll t;
+    cin>>t;
+    while(t--){
+        ll x,y,z;
+        cin>>x>>y>>z;
+
+        ll students = x*y;
+        if(students/2<z) yes;
+        else no;
+
+
+    }
+    return 0;
+}
