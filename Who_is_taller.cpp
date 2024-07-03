ea1c8ed22863b18bb3bd500a66b433c712a2afb2
Md Shojib Hossain <mdshojib922@gmail.com>
2024-07-03 17:53:42 +0600

    Taller Find

---
+++ Who_is_taller.cpp
@@ -0,0 +1,26 @@
+//Its_Shojib
+#include <bits/stdc++.h>
+using namespace std;
+#define mod 1000000007
+#define N 100005
+#define ll long long
+#define endl "\n"
+#define yes cout << "A\n"
+#define no cout << "B\n"
+long long fact[N];
+int main(){
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    ll t;
+    cin>>t;
+    while(t--){
+        ll x,y;
+        cin>>x>>y;
+        if(x>y){
+            yes;
+        }
+        else no;
+
+
+    }
+    return 0;
+}
