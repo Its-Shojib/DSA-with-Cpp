0821c4b44793a023c0f18fea1ba93bb2c093005c
Md Shojib Hossain <mdshojib922@gmail.com>
2024-07-05 00:39:08 +0600

    Games

---
+++ Chefland_Games.cpp
@@ -0,0 +1,22 @@
+//Its_Shojib
+#include <bits/stdc++.h>
+using namespace std;
+#define mod 1000000007
+#define N 100005
+#define ll long long
+#define endl "\n"
+#define yes cout << "IN\n"
+#define no cout << "OUT\n"
+long long fact[N];
+int main(){
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    ll t;
+    cin>>t;
+    while(t--){
+        ll a,b,c,d;
+        cin>>a>>b>>c>>d;
+        if(a == 0 && b == 0 && c == 0 && d == 0) yes;
+        else no;
+    }
+    return 0;
+}
