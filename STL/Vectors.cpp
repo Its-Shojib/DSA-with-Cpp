0d1803d114231e6b22459fdd09866472e73d3b3a
Md Shojib Hossain <mdshojib922@gmail.com>
2024-07-13 23:07:37 +0600

    Vector Learned

--- STL/Vectors.cpp
+++ STL/Vectors.cpp
@@ -9,6 +9,19 @@ using namespace std;
 #define no cout << "NO\n"
 int main(){
     ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-    
+    vector <ll> v;
+    ll n;
+    cin >> n;
+    for(ll i=1; i<=n; i++){
+        ll x;
+        cin >> x;
+        v.push_back(x);
+    }
+    reverse(v.begin(), v.end());
+    cout<<v.size()<<endl;
+    for(ll i=0; i<n; i++){
+        cout<<v[i]<<" ";
+    }
+    cout<<endl;
     return 0;
 }
