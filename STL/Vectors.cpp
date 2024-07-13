423f720acf8bf8a95c54da0c11f7082e1186b296
Md Shojib Hossain <mdshojib922@gmail.com>
2024-07-13 23:19:41 +0600

    Reference of a vector

--- STL/Vectors.cpp
+++ STL/Vectors.cpp
@@ -9,14 +9,21 @@ using namespace std;
 #define no cout << "NO\n"
 int main(){
     ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-    vector <ll> v;
+    vector <ll> v(5,3); // 5 size and fill with 3
     ll n;
     cin >> n;
     for(ll i=1; i<=n; i++){
         ll x;
         cin >> x;
         v.push_back(x);
+        //v.pop_back(); //remove the last value
     }
+
+    vector<ll> v2 =v; //copy the vector v into vector v2
+    // for(ll i=0; i<10; i++){
+    //     cout<<v2[i]<<" ";
+    // }
+
     reverse(v.begin(), v.end());
     cout<<v.size()<<endl;
     for(ll i=0; i<n; i++){
