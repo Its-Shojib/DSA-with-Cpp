a63327cf12bad507e97aa947f75fa22d72099512
Md Shojib Hossain <mdshojib922@gmail.com>
2024-07-14 11:50:48 +0600

    Linked List Travarsal

--- Linked List/Single_Linked_List.cpp
+++ Linked List/Single_Linked_List.cpp
@@ -16,8 +16,38 @@ struct Single_Linked_List
 
 Single_Linked_List *head = NULL;
 
+void insert(int value){
+    Single_Linked_List *newNode = new Single_Linked_List();
+    newNode-> data = value;
+    newNode-> next = head;
+    head = newNode;
+}
+
+void printList(){
+    Single_Linked_List *temp = head;
+    while(temp!= NULL){
+        cout << temp-> data << " ";
+        temp = temp-> next;
+    }
+    cout << endl;
+}
+
 int main(){
     ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    
+    // Creating a Singly Linked List with 6 nodes
+    insert(0);
+    printList();
+    insert(1);
+    printList();
+    insert(2);
+    printList();
+    insert(3);
+    printList();
+    insert(4);
+    printList();
+    insert(5);
+    printList();
 
     return 0;
 }
