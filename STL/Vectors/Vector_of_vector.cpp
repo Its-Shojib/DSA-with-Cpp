1bc0026eb79ed19f190609b32e991842a2cf39d1
Md Shojib Hossain <mdshojib922@gmail.com>
2024-07-15 14:47:01 +0600

    Vector of Vector

---
+++ STL/Vectors/Vector_of_vector.cpp
@@ -0,0 +1,42 @@
+// Its_Shojib
+#include <bits/stdc++.h>
+using namespace std;
+#define mod 1000000007
+#define N 100005
+#define ll long long
+#define endl "\n"
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int t;
+    cin >> t;
+
+    vector<vector<int>> v;
+    for (int i = 0; i < t; i++)
+    {
+        ll n;
+        cin >> n;
+        vector<int> temp;
+        for (int j = 0; j < n; j++)
+        {
+            int x;
+            cin >> x;
+            temp.push_back(x);
+        }
+        v.push_back(temp);
+    }
+    // this will print the vecor fo array
+    for (int i = 0; i < v.size(); i++)
+    {
+        for (int j = 0; j < v[i].size(); j++)
+        {
+            cout << v[i][j] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
