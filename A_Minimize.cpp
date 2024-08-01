dc29e73526a5ed5e9e5b925201bb6260aadcc065
Md Shojib Hossain <mdshojib922@gmail.com>
2024-08-01 23:17:08 +0600

    100 Level Problem

---
+++ A_Minimize.cpp
@@ -0,0 +1,20 @@
+//Its_Shojib
+#include <bits/stdc++.h>
+using namespace std;
+#define mod 1000000007
+#define N 100005
+#define ll long long
+#define endl "\n"
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+int main(){
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    ll t;
+    cin>>t;
+    while(t--){
+        ll x,y;
+        cin>>x>>y;
+        cout<<y-x<<endl;
+    }
+    return 0;
+}
