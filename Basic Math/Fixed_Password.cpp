e88320c7ca84d7f7ee2ca81cc2510c3a7a0c909e
Md Shojib Hossain <mdshojib922@gmail.com>
2024-09-11 23:17:06 +0600

    Basic math

---
+++ Basic Math/Fixed_Password.cpp
@@ -0,0 +1,28 @@
+// Its_Shojib
+#include <bits/stdc++.h>
+using namespace std;
+#define mod 1000000007
+#define N 100005
+#define ll long long
+#define endl "\n"
+#define yes cout << "Acesso Permitido\n"
+#define no cout << "Senha Invalida\n"
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    while (1)
+    {
+        ll n;
+        cin >> n;
+        if (n == 2002)
+        {
+            yes;
+            break;
+        }
+        else no;
+    }
+    return 0;
+}
